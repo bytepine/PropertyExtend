8e76b9559ac12266f340379bef7ce8ef20ba5a5e
byteyang <bytepine@gmail.com>
2025-12-17 16:28:18 +0800

    新建插件

---
+++ Source/PropertyExtend/Private/PropertyExtend.cpp
@@ -0,0 +1,21 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PropertyExtend.h"
+#include "PropertyEditorModule.h"
+#include "Modules/ModuleManager.h"
+
+#define LOCTEXT_NAMESPACE "FPropertyExtendModule"
+
+void FPropertyExtendModule::StartupModule()
+{
+	
+}
+
+void FPropertyExtendModule::ShutdownModule()
+{
+	
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FPropertyExtendModule, PropertyExtend)
